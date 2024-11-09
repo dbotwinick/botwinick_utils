58e45db52c6616f0c577bc54db74aa889a07cf12
Drew Botwinick <drew@drewbotwinick.com>
2024-11-09 12:10:23 -0600

    chore: version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", 'r') as f:
 
 setuptools.setup(
     name="botwinick_utils",
-    version="0.0.17",
+    version="0.0.18",
     author="Drew Botwinick",
     author_email="foss@drewbotwinick.com",
     description="Assorted Utilities and platform code",
